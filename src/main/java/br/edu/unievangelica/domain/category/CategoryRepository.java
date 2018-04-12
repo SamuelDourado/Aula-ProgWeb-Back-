cef8795e29f0193a9657c94dd94077b1c92f781d
SamuelDourado <sd7038@gmail.com>
2018-04-12 19:50:19 -0300

    first commit

---
+++ src/main/java/br/edu/unievangelica/domain/category/CategoryRepository.java
@@ -0,0 +1,7 @@
+package br.edu.unievangelica.domain.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository< Category, Long> {
+
+}
